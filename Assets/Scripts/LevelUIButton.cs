323b9d29e759288cb0029352ed4761f17f6912fb
James N <James@Apples-MacBook-Pro.local>
2017-04-15 14:56:31 +0100

    added LevelUIButton logic to display correct info.

---
+++ Assets/Scripts/LevelUIButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUIButton : MonoBehaviour {
+
+	[Header("Parameters")]
+	public LevelInfo thisLevel;
+	public LevelInfo previousLevel;
+	public int requiredScore;
+	public bool isUnlocked;
+
+
+	[Header("UI")]
+	public Image levelImage;
+	public Text levelNameText;
+	public Text statusText;
+	public Text requiredScoreText;
+	public Color unlockedColour;
+	public Color lockedColour;
+
+	private void Awake()
+	{
+		levelImage.sprite = thisLevel.stadiumSprite;
+		levelNameText.text = thisLevel.levelName;
+		if (isUnlocked)
+		{
+			statusText.text = "";
+			levelImage.color = unlockedColour;
+			requiredScoreText.text = "";
+		}
+		else
+		{
+			statusText.text = "LOCKED";
+			levelImage.color = lockedColour;
+			requiredScoreText.text = "SCORE " + requiredScore + " IN " + previousLevel.levelName + " TO UNLOCK";
+		}
+	}
+
+}
